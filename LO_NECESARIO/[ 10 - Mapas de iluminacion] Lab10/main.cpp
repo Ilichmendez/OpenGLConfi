968b347a24151c2b353558d047bd5052a8469aef
Elex03 <eacuna042@gmail.com>
2024-06-05 20:53:47 -0600

    add Projects

---
+++ LO_NECESARIO/[ 10 - Mapas de iluminacion] Lab10/main.cpp
@@ -0,0 +1,35 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error!" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+
+	const char* renderer = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+	std::cout << "Tarjeta gráfica: " << renderer << std::endl;
+
+	glfwTerminate();
+	return 0;
+}
